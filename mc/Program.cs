6b79206eaf4adbd4d2a0ad371fb50255c4df13d1
laoqu <quguiyou1076@qq.com>
2022-07-23 14:39:42 +0800

    This adds the basics we need, speccifically a lexer, a parser, and diagnostics that will lay the foundation for our compiler

--- mc/Program.cs
+++ mc/Program.cs
@@ -1,15 +1,80 @@
 ﻿// See https://aka.ms/new-console-template for more information
-while(true) {
+using Minsk.CodeAnalysis;
+
+static void PrettyPrint(SyntaxNode node, string indent = "", bool isLast = false)
+{
+    var marker = isLast ? "└──" : "├──";
+
+    Console.Write(indent);
+    Console.Write(marker);
+    Console.Write(" ");
+    Console.Write(node.Kind);
+
+    if (node is SyntaxToken t && t.Value != null)
+    {
+        Console.Write("   ");
+        Console.Write(t.Value);
+    }
+
+    Console.WriteLine();
+
+    indent += isLast ? " " : "│ ";
+
+    var lastChild = node.GetChildren().LastOrDefault();
+
+    foreach (var child in node.GetChildren())
+    {
+        PrettyPrint(child, indent, child == lastChild);
+    }
+}
+
+bool showTree = false;
+while (true)
+{
     Console.Write("> ");
     var line = Console.ReadLine();
-    if(string.IsNullOrWhiteSpace(line)) {
+    if (string.IsNullOrWhiteSpace(line))
+    {
         return;
     }
 
-    if(line == "1 + 2 * 3") {
-        Console.WriteLine("7");
+    if (line == "#showTree")
+    {
+        showTree = !showTree;
+        Console.WriteLine(showTree ? "Showing parse tree" : "Not showing parse tree");
+        continue;
     }
-    else {
-        Console.WriteLine("ERROR: Unknown expression");
+    else if (line == "#cls")
+    {
+        Console.Clear();
+        continue;
+    }
+
+    var syntaxTree = SyntaxTree.Parse(line);
+
+    if (showTree)
+    {
+        var color = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.DarkGray;
+        PrettyPrint(syntaxTree.Root);
+        Console.ForegroundColor = color;
+    }
+
+
+    if (syntaxTree.Diagnostics.Any())
+    {
+        var color = Console.ForegroundColor;
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        foreach (var diagnostic in syntaxTree.Diagnostics)
+        {
+            Console.WriteLine(diagnostic);
+        }
+        Console.ForegroundColor = color;
+    }
+    else
+    {
+        var e = new Evaluator(syntaxTree.Root);
+        var result = e.Evaluate();
+        Console.WriteLine(result);
     }
 }
