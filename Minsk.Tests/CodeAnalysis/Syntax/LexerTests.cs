08192dbd70bc50a0a856d30296f24120f787bc32
laoqu <quguiyou1076@qq.com>
2022-07-28 21:27:08 +0800

    Expose SyntaxFacts.GetText()

--- Minsk.Tests/CodeAnalysis/Syntax/LexerTests.cs
+++ Minsk.Tests/CodeAnalysis/Syntax/LexerTests.cs
@@ -2,7 +2,7 @@ using Minsk.CodeAnalysis.Syntax;
 
 namespace Minsk.Tests.CodeAnalysis.Syntax
 {
-    public class LexerTest
+    public class LexerTests
     {
         [Theory]
         [MemberData(nameof(GetTokensData))]
---
+++ Minsk.Tests/CodeAnalysis/Syntax/ParserTests.cs
@@ -0,0 +1,13 @@
+﻿using Minsk.CodeAnalysis.Syntax;
+
+namespace Minsk.Tests.CodeAnalysis.Syntax
+{
+    public class ParserTests
+    {
+        //[Theory]
+        //public void Parser_BinaryExpression_HonorsPrecedences(SyntaxKind op1, SyntaxKind op2)
+        //{
+
+        //}
+    }
+}
