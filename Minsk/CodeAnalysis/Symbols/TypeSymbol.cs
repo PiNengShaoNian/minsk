248333badb3a46e49bcf00363f8ce7ba1e3d7fd0
laoqu <quguiyou1076@qq.com>
2022-08-07 15:10:54 +0800

    Add abstract Symbol type

---
+++ Minsk/CodeAnalysis/Symbols/TypeSymbol.cs
@@ -0,0 +1,11 @@
+﻿namespace Minsk.CodeAnalysis.Symbols
+{
+    public sealed class TypeSymbol : Symbol
+    {
+        public TypeSymbol(string name) : base(name)
+        {
+        }
+
+        public override SymbolKind Kind => SymbolKind.Type;
+    }
+}
